f4b966b02fe8dd22f7dffc9538f16085d5178c44
xeliot <daveho8888@gmail.com>
2017-09-27 21:25:04 -0700

    play around with buttons

--- HelloSwift/HelloSwift/ViewController.swift
+++ HelloSwift/HelloSwift/ViewController.swift
@@ -20,8 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func showMessage() {
-        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertControllerStyle.alert)
+    @IBAction func snapMessage() {
+        let alertController = UIAlertController(title: "Snapchat button", message: "Hello Snap", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func robotMessage() {
+        let alertController = UIAlertController(title: "Robot button", message: "Hello Robot", preferredStyle: UIAlertControllerStyle.alert)
         alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
         present(alertController, animated: true, completion: nil)
     }
